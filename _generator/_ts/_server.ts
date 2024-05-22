ebb2613a3b2f8e8dc4f15b763035215e0ca911dc
finstory <facu995electro@hotmail.com>
2024-05-22 16:00:02 -0300

    plugin envs added

--- _generator/_ts/_server.ts
+++ _generator/_ts/_server.ts
@@ -1,25 +1,17 @@
-import dotenv from 'dotenv';
 import colors from 'colors';
 import server from '@config/server';
 import mongoDB from "@config/db/mongoDB/connection";
 import test from '@/test';
 import envs from '@envs';
-dotenv.config();
 
 
 //% SERVER CONFIG:
-// const PORT = process.env.PORT || 3000;
-const { PORT, CONNECT_DB } = envs;
-
-const connectDB = false;
-const initialDropDB = false;
-const testMode = true;
-
+const { PORT, CONNECT_DB, INITIAL_DROP_DB, TEST_MODE } = envs;
 
 //$ SERVER START:
 const upServer = () => {
   server.listen(PORT, () => { console.log(colors.italic(`Server listening on port ${PORT}`)) })
-  testMode && test();
+  TEST_MODE && test();
 }
 
-CONNECT_DB ? mongoDB(upServer, initialDropDB) : upServer();
+CONNECT_DB ? mongoDB(upServer, INITIAL_DROP_DB) : upServer();
